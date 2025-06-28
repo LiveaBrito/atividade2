e642d49bed9cdfcda7cb3b0fba06ca2144e20100
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2025-06-27 21:40:43 -0300

    Update calculadoraMediaEscolar.py

--- atividade2/calculadoraMediaEscolar.py
+++ atividade2/calculadoraMediaEscolar.py
@@ -6,9 +6,16 @@ Crie um programa que calcula a média escolar de um aluno. Use as seguintes nota
 * Nota 3: 6.5
 O programa deve calcular a média e exibir todas as notas e o resultado final, arredondando para duas casas decimais.'''
 
+'''
+Também pode usar desse jeito
 nota1 = 7.5
 nota2 = 8.0
 nota3 = 6.5
+'''
+
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+nota3 = float(input("Digite a nota 3: "))
 
 media = nota1 + nota2 + nota3 / 3
 
